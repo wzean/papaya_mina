20ead758cac1eb9db873dd415c703a1eb6cc4b3b
holazzer <751727648@qq.com>
2019-08-06 21:46:06 +0800

    too tired

--- pages/add_find/add_find.js
+++ pages/add_find/add_find.js
@@ -15,7 +15,35 @@ Page({
     // a find contains on more than 140 words.
     // fyi, no more than 9 pictures.
     console.log(e);
-  }
-
+  },
+  add_pic:e=>{
+    wx.chooseImage({
+      count:1,
+      sizeType:['compressed'],
+      sourceType:['album'],
+      success: function(res) {
+        var tempFilePaths = res.tempFilePaths
+        wx.uploadFile({
+          url: app.globalData.domain + '/upload/find_pic',
+          filePath: tempFilePaths[0],
+          name: 'pic', header: {
+            "Content-Type": "multipart/form-data"//记得设置
+          },
+          formData:{
+            'user_id':app.globalData.user_id,
+            token:app.globalData.token
+          },
+          success:res=>{
+            // save the iamge id and url somewhere for a find comit.
+          }
+        })
+      }
+        
+        })
+      },
+      commit:e=>{
+        
+      }
   
+
 })
